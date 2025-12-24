f5416de4719c001acac9d95786c72ea9ae2945e9
savar <skaar513@gmail.com>
2025-12-24 09:08:36 +0530

    yeah

--- pss-website/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Replace 'repo-name' with your actual GitHub repository name
-// If your repo is 'username.github.io', set base to '/'
-export default defineConfig({
-  plugins: [react()],
-  base: "/tools/pss-website/", 
-})
